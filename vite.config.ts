a674f414db2a2f47014e313b2eebc28182d1c073
curry <admin@MacBook-Pro.local>
2025-09-08 19:10:34 +0100

    add site map

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import sitemap from 'vite-plugin-sitemap'
+import posts from './src/posts.json'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const blogPostRoutes = posts.map((post: any) => `/blog/${post.slug}`);
+
+const staticRoutes = [
+  '/',
+  '/contact',
+  '/privacy',
+  '/terms',
+  '/security',
+  '/aboutus',
+  '/readycash',
+  '/readyscore',
+  '/readypay',
+  '/insights'
+];
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://fintech4esg.com',
+      dynamicRoutes: [...staticRoutes, ...blogPostRoutes],
+      // outDir: 'dist', // Par défaut, le sitemap est généré dans le dossier de build
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
